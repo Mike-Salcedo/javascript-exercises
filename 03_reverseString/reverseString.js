69712d3580571927258b3d7035c7ffb191b7ad59
Mike Salcedo <mikesalcedo191@gmail.com>
2023-03-26 01:25:13 -0400

    Updating with my solutions to Leap Year exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function (str) {
+  const slicedStr = str.split("");
+  let output = ``;
 
+  for (let i = slicedStr.length -1; i >= 0; i--) {
+    output += slicedStr[i];
+    
+  }
+    
+    return output;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
