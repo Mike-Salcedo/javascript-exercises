7ea451ce30bd91865caf78b051f8b31b075de2a0
Mike Salcedo <mikesalcedo191@gmail.com>
2023-05-29 14:06:36 -0400

    My solutions to the calculator exercises in the odin project

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function (numberA, numberB) {
+  return numberA + numberB;
 };
 
-const subtract = function() {
-	
+const subtract = function (numberA, numberB) {
+  return numberA - numberB;
 };
 
-const sum = function() {
-	
+const sum = function (args) {
+  return args.reduce((accumulator, current) => accumulator + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (args) {
+  return args.reduce((accumulator, current) => accumulator * current, 1);
 };
 
-const power = function() {
-	
+const power = function (numberA, numberB) {
+  return Math.pow(numberA, numberB);
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = 1;
+
+  for (let i = 1; i <= number; i++) result *= i;
+
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
