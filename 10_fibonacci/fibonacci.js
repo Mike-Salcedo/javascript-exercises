d43a8a84043620c9bb412d1ebb8580061158863a
Mike Salcedo <mikesalcedo191@gmail.com>
2023-06-07 07:59:06 -0400

    Resolution to fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  let fib;
+  switch (+number) {
+    case 0:
+      return +number;
+    case 1:
+      return +number;
+    default:
+      if (number <= -1) {
+        return "OOPS";
+      } else {
+        fib = fibonacci(number - 1) + fibonacci(number - 2);
+        return parseInt(fib);
+      }
+  }
 };
+
+console.log(fibonacci(10));
 
 // Do not edit below this line
 module.exports = fibonacci;
