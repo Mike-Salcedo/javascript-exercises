93299d4977d685dee35aa0259b19afc7262f07b8
Mike Salcedo <mikesalcedo191@gmail.com>
2023-06-10 21:36:05 -0400

    Submit my resolution for findTheOldest.js Exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  people.map((person) => { // If someone doesn't have a Year of Death make current year the Death date
+    if (person.yearOfDeath == undefined)
+      return (person.yearOfDeath = new Date().getFullYear());
+  });
 
+  return people.reduce((personA, personB) => { // Compare each persons years lived and return the oldest
+    const personOne = personA.yearOfDeath - personA.yearOfBirth;
+    const personTwo = personB.yearOfDeath - personB.yearOfBirth;
+    return personOne > personTwo ? personA : personB;
+  }, 0);
 };
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
